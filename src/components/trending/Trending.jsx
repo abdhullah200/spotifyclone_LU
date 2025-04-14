ac7c535ccee6aa1e9a469542a962229dae7f1334
rishabh32700 <rishabhpep@gmail.com>
2025-04-14 20:47:22 +0530

    first commit

---
+++ src/components/trending/Trending.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './trending.css'
+import TrendingCard from '../trendingCard/TrendingCard'
+
+const Trending = () => {
+  return (
+    <div className='trendingSectionMainContainer' >
+      <span>
+        Trending Songs
+      </span>
+      <TrendingCard />
+    </div>
+  )
+}
+
+export default Trending
---
+++ src/config/congif.js
@@ -0,0 +1,39 @@
+import img1 from '../assets/images/img1.png'
+import img2 from '../assets/images/img2.png'
+import img3 from '../assets/images/img3.png'
+import img4 from '../assets/images/img4.png'
+import img5 from '../assets/images/img5.png'
+import img6 from '../assets/images/img6.png'
+
+export const TrendingCardData = [
+    {
+      imgSrc: img1,
+      heading: "Shree Hanuman Chalisa",
+      subHeading: "Hariharan"
+    },
+    {
+      imgSrc: img2,
+      heading: "Keejo Kesari Ke Lal",
+      subHeading: "Lakhbir Singh Lakha"
+    },
+    {
+      imgSrc: img3,
+      heading: "Pal Pal",
+      subHeading: "Afusic, AliSoomroMusic"
+    },
+    {
+      imgSrc: img4,
+ heading: "Shree Hanuman Chalisa",
+      subHeading: "Hariharan"
+    },
+    {
+      imgSrc: img5,
+      heading: "Keejo Kesari Ke Lal",
+      subHeading: "Lakhbir Singh Lakha"
+    },
+    {
+      imgSrc: img6,
+      heading: "Pal Pal",
+      subHeading: "Afusic, AliSoomroMusic"
+    }
+  ]
