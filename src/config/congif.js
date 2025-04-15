cad41f3c98458bc50b02e2f71188f9d49231ead8
abdhullah200 <139129030+abdhullah200@users.noreply.github.com>
2025-04-16 01:44:55 +0530

    first commit

--- src/config/congif.js
+++ src/config/congif.js
@@ -10,53 +10,58 @@ import artist3 from "../assets/images/artist3.png";
 import artist4 from "../assets/images/artist4.png";
 import artist5 from "../assets/images/artist5.png";
 import artist6 from "../assets/images/artist6.png";
-import musicFile from '../assets/audio/1.mp3'
+import GalwayGirl from '../assets/audio/06. Galway Girl.mp3'
+import EndGame from '../assets/audio/02 End Game (feat. Ed Sheeran & Future).mp3'
+import GforBroke from '../assets/audio/Go for Broke (feat. James Arthur) - www.DLMp3Songs.Com - 128Kbps.mp3'
+import gobroke from '../assets/audio/11.  Gucci Mane -  Wake Up in the Sky.mp3'
+import Girlslikeyou from '../assets/audio/10.  Maroon 5 -  Girls Like You.mp3'
+import letme from '../assets/audio/02 End Game (feat. Ed Sheeran & Future).mp3'
 
 export const TrendingCardData = [
   {
     imgSrc: img1,
-    heading: "Shree Hanuman Chalisa",
-    subHeading: "Hariharan",
-    musicFile: musicFile,
+    heading: "Galway Girl",
+    subHeading: "Ed Sheeran",
+    musicFile: GalwayGirl,
   },
   {
     imgSrc: img2,
-    heading: "Keejo Kesari Ke Lal",
-    subHeading: "Lakhbir Singh Lakha",
-    musicFile: musicFile,
+    heading: "End Game",
+    subHeading: "feat. Ed Sheeran & Future",
+    musicFile: EndGame,
   },
   {
     imgSrc: img3,
-    heading: "Pal Pal",
-    subHeading: "Afusic, AliSoomroMusic",
-    musicFile: musicFile,
+    heading: "Go for Broke",
+    subHeading: "James Arthur",
+    musicFile: GforBroke,
   },
   {
     imgSrc: img4,
-    heading: "Shree Hanuman Chalisa",
-    subHeading: "Hariharan",
-    musicFile: musicFile,
+    heading: "Wake Up in the Sky",
+    subHeading: "Gucci Mane",
+    musicFile: gobroke,
   },
   {
     imgSrc: img5,
-    heading: "Keejo Kesari Ke Lal",
-    subHeading: "Lakhbir Singh Lakha",
-    musicFile: musicFile,
+    heading: "Girls Like You",
+    subHeading: "Maroon 5",
+    musicFile: Girlslikeyou,
   },
   {
     imgSrc: img6,
-    heading: "Pal Pal",
-    subHeading: "Afusic, AliSoomroMusic",
-    musicFile: musicFile,
+    heading: "Let Me",
+    subHeading: "ZAYN",
+    musicFile: letme,
   },
 ];
 
 export const artistList = [
-  { img: artist1, name: "Pritam" },
-  { img: artist2, name: "Arijit Singh" },
-  { img: artist3, name: "A.R. Rehman" },
-  { img: artist4, name: "Sachin-Jigar" },
-  { img: artist5, name: "Vishal-shekhar" },
-  { img: artist6, name: "Atif Aslam" },
-  { img: artist1, name: "Pritam" },
+  { img: artist1, name: "Alan Walker" },
+  { img: artist2, name: "Ariana Grande" },
+  { img: artist3, name: "Ed Sheeran" },
+  { img: artist4, name: "Kendrick Lamar" },
+  { img: artist5, name: "Niall Horan" },
+  { img: artist6, name: "Eminem" },
+  { img: artist1, name: "Alan Walker" },
 ];
